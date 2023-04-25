09cb5f0e297026b86eee56302e0e5b928331ab16
Alex Corn <alex@alexcorn.com>
2023-04-25 01:06:28 -0400

    Fixed BinaryGuardData schema not being properly used

--- src/protobufs.ts
+++ src/protobufs.ts
@@ -8,12 +8,15 @@ export function getProtoForMethod(apiInterface, apiMethod): {request: any, respo
 	let signature = [apiInterface, apiMethod].join('_');
 	let protoDefinitionName = PROTO_MAP[signature] || `C${signature}`;
 
-	let request = Protos[`${protoDefinitionName}_Request`];
-	let response = Protos[`${protoDefinitionName}_Response`];
+	let requestDefinitionName = `${protoDefinitionName}_Request`;
+	let responseDefinitionName = `${protoDefinitionName}_Response`;
 
-	if (signature == 'CAuthentication_BeginAuthSessionViaCredentials') {
-		request += '_BinaryGuardData';
+	if (signature == 'Authentication_BeginAuthSessionViaCredentials') {
+		requestDefinitionName += '_BinaryGuardData';
 	}
+
+	let request = Protos[requestDefinitionName];
+	let response = Protos[responseDefinitionName];
 
 	return {request, response};
 }
